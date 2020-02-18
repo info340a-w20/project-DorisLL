bc2ae3916d9517e6ca15b3d0963335a40b3cdc21
DorisLL <43691586+DorisLL@users.noreply.github.com>
2020-02-18 15:24:16 -0800

    Edit

--- js/Vaccine.js
+++ js/Vaccine.js
@@ -1,6 +1,6 @@
 'use strict';
+// Table/ List
 let newZip;
-
 document.querySelector("#button").addEventListener("click", (event) => {
     event.preventDefault();
     let value = newInputZip();
@@ -14,19 +14,7 @@ function newInputZip() {
     return inputNum;
 }
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/satellite-v9',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'pk.eyJ1IjoiZG9yaXNsIiwiYSI6ImNrNnNleGVudTAxbzAzZW5wMjR5b2JibTcifQ.EA3W2ouUwHcCW1uFNYxteg'
-  }).addTo(mymap);  
-
-
-  function myFunction(inputNum) {
+function myFunction(inputNum) {
     var input, table, tr, td, i, txtValue;
     input = inputNum;
     table = document.querySelector("table");
@@ -40,10 +28,22 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
         } else {
           tr[i].style.display = "none";
         }
-      }
-    //   if (count == tr.length - 1) {
-    //     let text = "Sorry, there is no data from the area now."
-    //     console.log(text);
-    //   }     
+      }  
     }
   }
+
+  //Map
+
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+//   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//       maxZoom: 18,
+//       id: 'mapbox/satellite-v9',
+//       tileSize: 512,
+//       zoomOffset: -1,
+//       accessToken: 'pk.eyJ1IjoiZG9yaXNsIiwiYSI6ImNrNnNleGVudTAxbzAzZW5wMjR5b2JibTcifQ.EA3W2ouUwHcCW1uFNYxteg'
+//   }).addTo(mymap);  
+
