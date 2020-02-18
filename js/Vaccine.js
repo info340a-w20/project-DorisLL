fed243bb1630dc6c03f88fe6d2b2693c1c71e0a0
DorisLL <43691586+DorisLL@users.noreply.github.com>
2020-02-18 15:05:14 -0800

    List works

--- js/Vaccine.js
+++ js/Vaccine.js
@@ -2,45 +2,20 @@
 let newZip;
 
 document.querySelector("#button").addEventListener("click", (event) => {
-    let inputNum;
     event.preventDefault();
-    newInputZip(inputNum);
-    console.log(inputNum);
-    myFunction(inputNum);
+    let value = newInputZip();
+    console.log(value);
+    myFunction(value);
+    document.querySelector('input').value ='';
 })
 
-function newInputZip(inputNum) {
-    inputNum = document.querySelector("input").valueAsNumber;
-    console.log(inputNum);
+function newInputZip() {
+    let inputNum = document.querySelector("input").valueAsNumber;
     return inputNum;
 }
 
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-// function myFunction() {
-//     // Declare variables
-//     var input, filter, table, tr, td, i, txtValue;
-//     input = newZip;
-//     filter = input.value.toUpperCase();
-//     table = document.getElementById("#locationMap");
-//     tr = table.getElementsByTagName("tr");
-  
-//     // Loop through all table rows, and hide those who don't match the search query
-//     for (i = 0; i < tr.length; i++) {
-//       td = tr[i].getElementsByTagName("td")[0];
-//       if (td) {
-//         txtValue = td.textContent || td.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//           tr[i].style.display = "";
-//         } else {
-//           tr[i].style.display = "none";
-//         }
-//       }
-//     }
-//   }
-
-  var mymap = L.map('#locationMap').setView([51.505, -0.09], 13);
-  // console.log(mymap);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
@@ -56,7 +31,7 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
     input = inputNum;
     table = document.querySelector("table");
     tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
+    for (i = 1; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[3];
       if (td) {
         txtValue = Number(td.textContent);
@@ -65,6 +40,10 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
         } else {
           tr[i].style.display = "none";
         }
-      }       
+      }
+    //   if (count == tr.length - 1) {
+    //     let text = "Sorry, there is no data from the area now."
+    //     console.log(text);
+    //   }     
     }
   }
