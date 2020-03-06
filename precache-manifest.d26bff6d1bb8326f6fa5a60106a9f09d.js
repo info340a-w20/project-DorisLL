b9f245b22d2c344a671d6ebe740701a360f76e7b
MACKENZIE HUTCHISON <mack2@uw.edu>
2020-03-06 01:27:34 -0800

    Updates

---
+++ precache-manifest.d26bff6d1bb8326f6fa5a60106a9f09d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c66f64a081c48480f1efaf71bfa6db0",
+    "url": "/react-DorisLL/index.html"
+  },
+  {
+    "revision": "ad2dbc2e37d18ed81560",
+    "url": "/react-DorisLL/static/css/2.10c3ffbc.chunk.css"
+  },
+  {
+    "revision": "3c2b115f94824329aa52",
+    "url": "/react-DorisLL/static/css/main.bf14de78.chunk.css"
+  },
+  {
+    "revision": "ad2dbc2e37d18ed81560",
+    "url": "/react-DorisLL/static/js/2.c634a876.chunk.js"
+  },
+  {
+    "revision": "60cb8bbb629127159ec6eeb4dffb57ad",
+    "url": "/react-DorisLL/static/js/2.c634a876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2b115f94824329aa52",
+    "url": "/react-DorisLL/static/js/main.9ee9e0b1.chunk.js"
+  },
+  {
+    "revision": "6ae22336f329eec60a8b",
+    "url": "/react-DorisLL/static/js/runtime-main.03ecb10c.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/react-DorisLL/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/react-DorisLL/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/react-DorisLL/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/react-DorisLL/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/react-DorisLL/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/react-DorisLL/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/react-DorisLL/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/react-DorisLL/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/react-DorisLL/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/react-DorisLL/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/react-DorisLL/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/react-DorisLL/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/react-DorisLL/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/react-DorisLL/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/react-DorisLL/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/react-DorisLL/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/react-DorisLL/static/media/fa-solid-900.cdfec5cf.woff"
+  }
+]);
