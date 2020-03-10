c69a4dffe18dd96cd2cc93e629c8b06997f6a915
DorisLL <43691586+DorisLL@users.noreply.github.com>
2020-03-10 15:24:33 -0700

    Fix inputBox

--- src/InputBox.js
+++ src/InputBox.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+// import TextField from '@material-ui/core/TextField';
 import './css/WebpageStyle.css'
 
 
 
-export class InputBox extends React.Component {
 
+export class InputBox extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        tags: []
+      };
+      this.onTagsChange = this.onTagsChange.bind(this);
+    }
   
+    onTagsChange = (event, values) => {
+      this.setState({
+        tags: values
+      }, () => {
+        // This will output an array of objects
+        // given by Autocompelte options property.
+        console.log(this.state.tags);
+      });
+    }
 
     render() {
-
-      const list= require('./data/csvjson.json')
-        // const list = [
-        //     { title: 'The Shawshank Redemption', year: 1994 },
-        //     {Name:'Walgreens', Address: '4710 S WESTERN AVE', City: 'Chicago', Latitude: 41.807899, Longitude: -87.684699, ZIP: 60609},
-        //     {Name:'19th Ward Office', Address: '10402 S. Western Ave.', City: 'Chicago', Latitude: 41.70439, Longitude: -87.68197, ZIP: 60643},
-        //     {Name:'20th Ward-Whole Foods Family Flu Day', Address: '832 W. 63rd St.', City: 'Chicago', Latitude: 41.78059, Longitude: -87.64621, ZIP: 60621},
-        //     {Name:'Walgreens', Address: '711 W NORTH AVE STE 204', City: 'Chicago', Latitude: 41.91082, Longitude: -87.64665, ZIP: 60610}
-        //   ];
+      // const list= require('./data/csvjson.json')
+        const list = [
+            { title: 'The Shawshank Redemption', year: 1994 },
+            {Name:'Walgreens', Address: '4710 S WESTERN AVE', City: 'Chicago', Latitude: 41.807899, Longitude: -87.684699, ZIP: 60609},
+            {Name:'19th Ward Office', Address: '10402 S. Western Ave.', City: 'Chicago', Latitude: 41.70439, Longitude: -87.68197, ZIP: 60643},
+            {Name:'20th Ward-Whole Foods Family Flu Day', Address: '832 W. 63rd St.', City: 'Chicago', Latitude: 41.78059, Longitude: -87.64621, ZIP: 60621},
+            {Name:'Walgreens', Address: '711 W NORTH AVE STE 204', City: 'Chicago', Latitude: 41.91082, Longitude: -87.64665, ZIP: 60610}
+          ];
         return (
-          // <div >
+          <div >
               <Autocomplete
                   id="input-box"
                   options={list}
@@ -28,8 +44,7 @@ export class InputBox extends React.Component {
                   style={{ width: "100%" }}
                   renderInput={params => <TextField {...params} label="Zipcode" variant="outlined" />}
               />
-           
-          // </div>
+          </div>
             
         );
     }
