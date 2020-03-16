117b808e2522b0a5913f1240368105a0c19bf8a3
MACKENZIE HUTCHISON <mack2@uw.edu>
2020-03-16 15:19:08 -0700

    update estimates page

--- src/estimatePage.js
+++ src/estimatePage.js
@@ -21,10 +21,14 @@ export class EstimatePage extends React.Component {
     }
   }
 
+  // componentDidMount() {
+  //   // Store the AuthObserver (so you can unauthorize the application)
+  //   this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+  //     (user) => this.setState({isSignedIn: !!user})
+  // );
+
   render() {
-      const data = [{name: 'Your Estimate', value: 400}, {name: 'Average Estimate', value: 400}, {name: 'Actual Value', value: 400}];
-      console.log("current state: " + this.state.guessedInc
-                  + "allguesses: " + this.state.allInc);
+
       return (
         <div id="content">
           <PageHeader />
@@ -32,8 +36,8 @@ export class EstimatePage extends React.Component {
             cardHeader: "How Does the Flu Affect Us?",
             cardText: "Dangers of the flu are very commonly overlooked, due to the normalization of the illness. It is important for people to understand the need for vaccinations, in the context of how dangerous these outbreaks can actually be. Despite being an illness which is present each year, it can still have drastic effects on our community, if we forego precautionary measures."      
           }]}/>
-          <Hospitalizations allGuesses={[100, 300, 600, 642, 796, 88]} actualValue={406.06} inputs={[{'guessedHosp': "Estimate the average number of hospitalizations from the flu per population of 100,000: "}]} paragraph={"Despite the current quality of health care, influenza does still have a serious effect on the health of our communities. Despite the fact that the flu is not generally considered fatal, hospitalizations occur due to the illness every year, which people do not recognize. Test your perception of flu related hospitalizations against real data from last year's flu season below."}/>
-          <Hospitalizations allGuesses={[1, 2, 3, 5, 2, 5]} actualValue={3} inputs={[{'guessedInc': "Estimate the average number of days a disease is incubated: "}]} paragraph={"Dangers of the flu are very commonly overlooked, due to the normalization of the illness. It is important for people to understand the need for vaccinations, in the context of how dangerous these outbreaks can actually be. Despite being an illness which is present each year, it can still have drastic effects on our community, if we forego precautionary measures."}/>
+          <Hospitalizations dataRef={'hospitalizations'} actualValue={406.06} inputs={[{'guessedHosp': "Estimate the average number of hospitalizations from the flu per population of 100,000: "}]} paragraph={"Despite the current quality of health care, influenza does still have a serious effect on the health of our communities. Despite the fact that the flu is not generally considered fatal, hospitalizations occur due to the illness every year, which people do not recognize. Test your perception of flu related hospitalizations against real data from last year's flu season below."}/>
+          <Hospitalizations dataRef={'incubation'} actualValue={3} inputs={[{'guessedInc': "Estimate the average number of days a disease is incubated: "}]} paragraph={"Dangers of the flu are very commonly overlooked, due to the normalization of the illness. It is important for people to understand the need for vaccinations, in the context of how dangerous these outbreaks can actually be. Despite being an illness which is present each year, it can still have drastic effects on our community, if we forego precautionary measures."}/>
           <PageFooter />
         </div>
       );
--- src/hospitalizations.js
+++ src/hospitalizations.js
@@ -2,52 +2,119 @@ import React from 'react';
 import {InputForms} from './InputForms.js'
 import {EstimateBarChart} from './barChart.js'
 import './css/WebpageStyle.css'
+import firebase from "firebase";
+import 'firebase/database';
+
 
 //this.props.allGuesses
 //this.props.actualValue
 //this.props.inputs
-
 //inputs=[{'guessedInc': "The average number of days a disease is incubated is "}]
+
 export class Hospitalizations extends React.Component {
+//   super(props)
+//   this.submissionsRef = firebase.database().ref(this.props.dataRef);
+//   const userRef = this.submissionsRef.child(firebase.auth().currentUser.uid);
+  // userRef.on('value').then(function(snap) {
+  //   if(snap.val() === null) {
+  //     this.state = {
+  //       inputtedGuess: '',
+  //       guessedInc: 0,
+  //       allInc: [],
+  //       renderedAverage: 0,
+  //       actualInc: 0
+  //     }
+  //   }
+  // })
+
     constructor(props) {
         super(props)
         this.state = {
           inputtedGuess: '',
           guessedInc: 0,
-          allInc: this.props.allGuesses,
+          allInc: [],
           renderedAverage: 0,
-          actualInc: 0
+          actualInc: 0,
+          submitted: false
         }
     }
     
+    componentDidMount() {
+      this.submissionsRef = firebase.database().ref(this.props.dataRef);
+      this.submissionsRef.on("value", (snapshot) => {
+        let submissionsObject = snapshot.val();
+        let submissionKeys = Object.keys(submissionsObject);
+        //add value each to an array
+        let submissionArray = submissionKeys.map((key) => {
+          let submission = submissionsObject[key];
+          return submission.value;
+        })
+
+        //this.setState({allInc: submissionArray});
+        
+        //check to see if the keys array contains the userid
+        //set the state to default values if they do no exist in the database
+        //if they do exist, set the state to their guess, and the rendered values (leave extimates unmodified to the updated snapshot version)
+        
+        let user = firebase.auth().currentUser.uid;
+        if(submissionsObject[user] != undefined) {
+          console.log("user found")
+          let userObject = submissionsObject[user];
+          let userGuess = userObject.value;
+          console.log("userGuess: " + userGuess);
+          this.setState({
+            guessedInc: userGuess,
+            allInc: submissionArray,
+            actualInc: this.props.actualValue,
+            submitted: true
+          });
+          console.log("user found state: ", this.state);
+        }
+        //  else {
+        //   console.log("user not found")
+        //   this.state = {
+        //     guessedInc: 0,
+        //     allInc: [],
+        //     actualInc: 0
+        //   }
+        // }
+
+        console.log("state: ", this.state);
+    })
+
+    }
+
     handleChange = (event) => {
-        console.log(event.target.value)
         this.setState({inputtedGuess: event.target.value});
       }
     
       handleSubmit = (event) => {
-        console.log("submitted");
+        // when submitted, add to the db
+        const userRef = this.submissionsRef.child(firebase.auth().currentUser.uid);
         let submittedValue = Number(this.state.inputtedGuess);
-        let newArray = this.state.allInc;
-        newArray.push(submittedValue)
-        let newAverage = (newArray.reduce((a,b) => a + b, 0) / newArray.length)
-        console.log(newAverage)
-        this.setState({
-          inputtedGuess: '',
-          guessedInc: submittedValue,
-          allInc: newArray,
-          renderedAverage: newAverage,
-          actualInc: this.props.actualValue
-        })
+        userRef.set({
+          value: submittedValue,
+        });
+
       }
     
     render(){
-        return(
+        this.submissionsRef = firebase.database().ref(this.props.dataRef);
+        console.log("rendered state: ", this.state);
+        let newAverage = (this.state.allInc.reduce((a,b) => a + b, 0) / this.state.allInc.length)
+        if (this.state.submitted) {
+          return(
+            <EstimateBarChart inputtedGuess={this.state.guessedInc} averageGuess={newAverage} actualValue={this.state.actualInc}/>
+          )
+        } else {
+          return(
             <div>
                 <p id="descriptive-paragraph">{this.props.paragraph}</p>
-                <InputForms inputs={this.props.inputs} onUpdate={this.handleChange.bind(this)} onClick={this.handleSubmit.bind(this)}/>
-                <EstimateBarChart inputtedGuess={this.state.guessedInc} averageGuess={this.state.renderedAverage} actualValue={this.state.actualInc}/>
+                <InputForms style={{visibility: 'hidden'}} inputs={this.props.inputs} onUpdate={this.handleChange.bind(this)} onClick={this.handleSubmit.bind(this)}  />
+                <EstimateBarChart inputtedGuess={this.state.guessedInc} averageGuess={newAverage} actualValue={this.state.actualInc}/>
             </div>
-        )
+          )
+        }        
+
     }    
 }
